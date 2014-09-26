5b2e45824b565061cb6fa5979b0577803df97433
Tim Jarratt <tjarratt@gmail.com>
2014-09-25 20:59:54 -0700

    photo-picker will present a UIImagePickerController
    Pretty simple not too hard to configure. Could use a delegate to handle
    selection, and maybe some choices for camera versus photo library.
    Could also use some cedar tests.

--- photo-picker/photo-picker/photo-picker/GLGViewController.m
+++ photo-picker/photo-picker/photo-picker/GLGViewController.m
@@ -14,16 +14,11 @@
 
 @implementation GLGViewController
 
-- (void)viewDidLoad
+- (IBAction)didTapSelectPhotoButton:(id)sender
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    UIImagePickerController *controller = [[UIImagePickerController alloc] init];
+    [controller setModalPresentationStyle:UIModalPresentationCurrentContext];
+    [self presentViewController:controller animated:YES completion:nil];
 }
 
 @end
